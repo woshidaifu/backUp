4fae1c0dcdf979798631c203341d29e107de9319
daifu <pflee@live.com>
2025-04-20 05:57:57 +0800

    add centos7 dockerfile and tool script

---
+++ centos7/run.sh
@@ -0,0 +1,2 @@
+docker run -d --name centos7-dev  --privileged -p 2222:22 daifu/centos7-devbase
+
---
+++ getCookies.js
@@ -0,0 +1,26 @@
+// 获取指定名称的cookie
+function getCookie(name){
+    var strcookie = document.cookie;//获取cookie字符串
+    var arrcookie = strcookie.split("; ");//分割
+    //遍历匹配
+    for ( var i = 0; i < arrcookie.length; i++) {
+        var arr = arrcookie[i].split("=");
+        if (arr[0] == name){
+            return arr[1];
+        }
+    }
+    return "";
+}
+
+// 打印所有cookie
+function print() {
+    var strcookie = document.cookie;//获取cookie字符串
+    var arrcookie = strcookie.split(";");//分割
+
+    //遍历匹配
+    for ( var i = 0; i < arrcookie.length; i++) {
+        var arr = arrcookie[i].split("=");
+        console.log(arr[0] +"=" + arr[1]+";");
+    }
+}
+print();
